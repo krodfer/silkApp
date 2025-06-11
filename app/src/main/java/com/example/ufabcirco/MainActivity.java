3632a1287c27c9d430e80225e8b6eb7ad1cf9929
Travon Rodfer <kaiq.ferrer@gmail.com>
2025-06-11 18:13:18 -0300

    Adding icons

--- app/src/main/java/com/example/ufabcirco/MainActivity.java
+++ app/src/main/java/com/example/ufabcirco/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.ufabcirco;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+import com.example.ufabcirco.adapter.SectionsPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
-import com.example.ufabcirco.adapter.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), getLifecycle());
         ViewPager2 viewPager = findViewById(R.id.view_pager_main);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), getLifecycle());
         viewPager.setAdapter(sectionsPagerAdapter);
 
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        new TabLayoutMediator(tabLayout, viewPager,
-                (tab, position) -> tab.setText(sectionsPagerAdapter.getPageTitle(position))
-        ).attach();
+        viewPager.setUserInputEnabled(false);
+
+        int[] tabIcons = {
+                R.drawable.ic_fila,
+                R.drawable.ic_tabela,
+                R.drawable.ic_galeria
+        };
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            tab.setText(sectionsPagerAdapter.getPageTitle(position));
+            tab.setIcon(tabIcons[position]);
+        }).attach();
     }
 }
